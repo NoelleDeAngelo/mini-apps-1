c371473ced86f9e5b24a5bc17f898164f0d2c515
Noelle DeAngelo <NoelleDeAngelo@gmail.com>
2021-04-05 10:53:14 -0700

    style board to show borders of playspaces

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,16 @@
 
+var currentPlayer = 'x';
 
+
+var handleBoardClick= function (event) {
+  console.log('hi')
+  console.log(event);
+};
+
+var checkForWin = function (){
+
+};
+
+var checkForTie = function () {
+  // might not nees this, if there is no winner and board is full it is a tie
+}

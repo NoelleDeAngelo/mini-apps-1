600b41cbcf4f55abb923e08b5116311a3e916f34
Noelle DeAngelo <NoelleDeAngelo@gmail.com>
2021-04-05 14:34:39 -0700

    Refactor handling of wins into seperate function

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -6,9 +6,11 @@ boardSpaces.forEach((element)=> element.addEventListener('click', (event)=>handl
 var reset = document.getElementById('reset');
 reset.addEventListener('click', (event)=> handleReset());
 
-var currentPlayer = 'X';
+var gameState = {
 
-var boardState = {
+currentPlayer:'X',
+
+boardState: {
   space1:null,
   space2:null,
   space3:null,
@@ -18,17 +20,25 @@ var boardState = {
   space7:null,
   space8:null,
   space9:null
+},
+
+winner: null,
+
+wins:{
+  X:0,
+  O:0
 }
- var winner= null;
+
+}
 
 var handleBoardClick= function (event) {
   //console.log(event)
-  event.target.innerHTML= currentPlayer;
-  boardState[event.target.attributes.id.value]= currentPlayer;
-  //console.log(boardState)
+  event.target.innerHTML= gameState.currentPlayer;
+  gameState.boardState[event.target.attributes.id.value]= gameState.currentPlayer;
+  //console.log(gameState.boardState)
   checkForWin();
   checkForTie()
-  currentPlayer === 'X'? currentPlayer="O" : currentPlayer= 'X';
+  gameState.currentPlayer === 'X'? gameState.currentPlayer="O" : gameState.currentPlayer= 'X';
 };
 
 var checkForWin = function (){
@@ -45,17 +55,14 @@ var checkForWin = function (){
 
 
 var rowsHaveWinner= function() {
-  if(boardState.space1 === boardState.space2 && boardState.space1 === boardState.space3 &&  boardState.space1 !==null) {
-    alert(`player ${boardState.space1} has won!`);
-    winner= boardState.space1;
+  if(gameState.boardState.space1 === gameState.boardState.space2 && gameState.boardState.space1 === gameState.boardState.space3 &&  gameState.boardState.space1 !==null) {
+    handleWin(gameState.boardState.space1)
     return true;
-  }else if(boardState.space4 === boardState.space5 && boardState.space4 === boardState.space6 &&  boardState.space4 !==null) {
-    alert(`player ${boardState.space4} has won!`);
-    winner= boardState.space4;
+  }else if(gameState.boardState.space4 === gameState.boardState.space5 && gameState.boardState.space4 === gameState.boardState.space6 &&  gameState.boardState.space4 !==null) {
+    handleWin(gameState.boardState.space4)
     return true;
-  }else if(boardState.space7 === boardState.space8 && boardState.space7 === boardState.space9 &&  boardState.space7 !==null) {
-    alert(`player ${boardState.space7} has won!`);
-    winner= boardState.space7;
+  }else if(gameState.boardState.space7 === gameState.boardState.space8 && gameState.boardState.space7 === gameState.boardState.space9 &&  gameState.boardState.space7 !==null) {
+    handleWin(gameState.boardState.space7)
     return true;
   }else {
     return false;
@@ -63,17 +70,14 @@ var rowsHaveWinner= function() {
 }
 
 var coulmnsHaveWinner= function() {
-  if(boardState.space1 === boardState.space4 && boardState.space1 === boardState.space7 &&  boardState.space1 !==null) {
-    alert(`player ${boardState.space1} has won!`);
-    winner= boardState.space1;
+  if(gameState.boardState.space1 === gameState.boardState.space4 && gameState.boardState.space1 === gameState.boardState.space7 &&  gameState.boardState.space1 !==null) {
+    handleWin(gameState.boardState.space1)
     return true;
-  }else if(boardState.space2 === boardState.space5 && boardState.space2 === boardState.space8 &&  boardState.space2 !==null) {
-    alert(`player ${boardState.space2} has won!`);
-    winner= boardState.space2;
+  }else if(gameState.boardState.space2 === gameState.boardState.space5 && gameState.boardState.space2 === gameState.boardState.space8 &&  gameState.boardState.space2 !==null) {
+    handleWin(gameState.boardState.space2)
     return true;
-  }else if(boardState.space3 === boardState.space6 && boardState.space3 === boardState.space9 &&  boardState.space3 !==null) {
-    alert(`player ${boardState.space3} has won!`);
-    winner= boardState.space3;
+  }else if(gameState.boardState.space3 === gameState.boardState.space6 && gameState.boardState.space3 === gameState.boardState.space9 &&  gameState.boardState.space3 !==null) {
+    handleWin(gameState.boardState.space3)
     return true;
   }else {
     return false;
@@ -82,13 +86,11 @@ var coulmnsHaveWinner= function() {
 }
 
 var diagonalsHaveWinner= function() {
-  if(boardState.space1 === boardState.space5 && boardState.space1 === boardState.space9 &&  boardState.space1 !==null) {
-    alert(`player ${boardState.space1} has won!`);
-    winner= boardState.space1;
+  if(gameState.boardState.space1 === gameState.boardState.space5 && gameState.boardState.space1 === gameState.boardState.space9 &&  gameState.boardState.space1 !==null) {
+    handleWin(gameState.boardState.space1)
     return true;
-  }else if(boardState.space3 === boardState.space5 && boardState.space3 === boardState.space7 &&  boardState.space3 !==null) {
-    alert(`player ${boardState.space3} has won!`);
-    winner= boardState.space3;
+  }else if(gameState.boardState.space3 === gameState.boardState.space5 && gameState.boardState.space3 === gameState.boardState.space7 &&  gameState.boardState.space3 !==null) {
+    handleWin(gameState.boardState.space3)
     return true;
   }else {
     return false;
@@ -96,9 +98,14 @@ var diagonalsHaveWinner= function() {
 
 }
 
+var handleWin = function (winner){
+  gameState.winner=winner;
+  alert(`player ${winner} has won!`);
+}
+
 var checkForTie = function () {
-  for (space in boardState){
-    if(boardState[space]=== null){
+  for (space in gameState.boardState){
+    if(gameState.boardState[space]=== null){
       return;
     }
   }
@@ -107,8 +114,8 @@ var checkForTie = function () {
 
 var handleReset= function () {
   boardSpaces.forEach((element)=> element.innerHTML= '');
-  for (space in boardState) {
-    boardState[space]= null;
+  for (space in gameState.boardState) {
+    gameState.boardState[space]= null;
   }
-  currentPlayer= winner || 'X';
+  gameState.currentPlayer= gameState.winner || 'X';
 }

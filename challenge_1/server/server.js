28a1bdeca0df3cc0acee031f2f4151a3643ac9c8
Noelle DeAngelo <NoelleDeAngelo@gmail.com>
2021-04-05 18:58:59 -0700

    Set up server snd serve index page

--- challenge_1/server/server.js
+++ challenge_1/server/server.js
@@ -1,11 +1,12 @@
 const express = require ('express');
 const app = express();
-const index = require('../client/index.html');
+//const index = require('../views/index.html');
 
-
+app.use(express.static('public'))
+app.engine('html', require('ejs').renderFile);
 
 app.get('/', (req, res)=>{
-  res.send(' this is sent')
+  res.render('/Users/noelledeangelo/Hack Reactor/week5/hr-sjo5-mini-apps-1/challenge_1/views/index.html')
 })
 
 app.listen(3000, ()=> console.log('listening on 3000'));

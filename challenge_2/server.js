f599b28f118d7ee7072d6c124e300de0c908937a
Noelle DeAngelo <NoelleDeAngelo@gmail.com>
2021-04-06 11:58:05 -0700

    Add route for post from form

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -8,6 +8,10 @@ App.get('/', (req, res)=>{
   res.render('index.html')
 })
 
+App.post('/', (req, res)=>{
+  console.log('something was posted')
+  
+})
 
 App.listen(3030,()=>console.log(' App is listening on 3030') )
 

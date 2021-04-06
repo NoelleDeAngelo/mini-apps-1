3dff8b57f2d2e02f5f941af260e70cc14bdb8c58
Noelle DeAngelo <NoelleDeAngelo@gmail.com>
2021-04-06 11:28:19 -0700

    Set up express server

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,11 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const App = express();
+
+
+App.use(express.static('client'))
+
+
+App.listen(3030,()=>console.log(' App is listening on 3030') )
 
 

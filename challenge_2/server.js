f5b95be93a3659928ff8321073f63cddd876d454
Noelle DeAngelo <NoelleDeAngelo@gmail.com>
2021-04-06 11:38:45 -0700

    Set out route to serve index Please enter the commit message for your changes. Lines starting

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -2,8 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const App = express();
 
-
 App.use(express.static('client'))
+
+App.get('/', (req, res)=>{
+  res.render('index.html')
+})
 
 
 App.listen(3030,()=>console.log(' App is listening on 3030') )

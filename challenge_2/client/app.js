b0ab0713b625789074b8234b7d12c677c8504a65
Noelle DeAngelo <NoelleDeAngelo@gmail.com>
2021-04-06 16:44:34 -0700

    Add CreateCSV method to turn Json to CSV

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,53 @@
 
 
+ var createCSV= function (text){
+    var obj= JSON.parse(text);
+    console.log(obj);
+    var storage= [];
+    var result=[];
+    var inner= function(obj) {
+      for (key in obj){
+      if(key === 'children'){
+        storage.pop()
+        result.push(storage.join(''))
+        storage=[];
+        if( obj[key].length > 0){
+          for (var i=0; i< obj[key].length; i++){
+          inner(obj[key][i], storage);
+          }
+        }
+      }else{
+      storage.push(obj[key])
+      storage.push(',')
+      }
+    }
+
+    }
+    inner(obj)
+    for (key in obj){
+      storage.push(key);
+      storage.push(',');
+    }
+    storage.pop()
+    result.unshift(storage.join(''));
+    result=result.join('\n')
+
+    console.log(result)
+  }
+
+
+module.exports= {createCSV}
+
+
+
+//key === children && obj[children].length > 0
+
+
+// {
+//   "firstName": "Joshie",
+//   "lastName": "Wyattson",
+//   "county": "San Mateo",
+//   "city": "San Mateo",
+//   "role": "Broker",
+//   "sales": 1000000
+// }
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,17 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const multer = require('multer')
+const app= require('./client/app.js')
+const upload= multer()
 const App = express();
 
 App.use(express.static('client'))
 
+
+
 App.get('/', (req, res)=>{
-  res.render('index.html')
+  res.sendFile('index.html')
 })
 
-App.post('/', (req, res)=>{
-  console.log('something was posted')
-  
+App.post('/', upload.single('file'), (req, res)=>{
+  console.log(app)
+  app.createCSV(req.body.JSONText)
+
+  res.sendFile('/Users/noelledeangelo/Hack Reactor/week5/hr-sjo5-mini-apps-1/challenge_2/client/index.html')
+
 })
+
 
 App.listen(3030,()=>console.log(' App is listening on 3030') )
 

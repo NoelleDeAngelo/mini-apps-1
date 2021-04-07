56f4f8403fba3d25ffc9db35307edee9e55b1d31
Noelle DeAngelo <NoelleDeAngelo@gmail.com>
2021-04-07 12:12:03 -0700

    set up index.html to render app

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,60 @@
 
+class App extends React.Component {
+  constructor (props){
+    super(props)
+    this.state= {
+      whatToRender: Form1
+    }
+  }
+  render(){
+    return (
+      <this.state.whatToRender/>
+    )
+  }
 
+
+
+}
+
+var Home = function (props){
+  return (
+  <div>
+    <h1>Checkout Cart</h1>
+    <button>checkout</button>
+  </div>
+
+  )
+}
+
+var Form1 = function (props){
+  return (<div>
+    <h1>Sign Up</h1>
+    <form>
+      <lable>Name:
+        <input required id='name'></input>
+      </lable>
+      <lable>Email:
+        <input required type='email'></input>
+      </lable>
+      <lable>Password:
+        <input required type='password'></input>
+      </lable>
+      <button >Sign Up</button>
+    </form>
+  </div>
+
+  )
+}
+var Form2 = function (props){
+  return (
+    <button>checkout</button>
+  )
+}
+
+
+
+
+
+
+
+ReactDOM.render(<App/>, document.getElementById('app'))
